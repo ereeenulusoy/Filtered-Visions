1d631eb107059cef0e0d2c5d876fdaa28610cb94
trexbest <cndgn3737@gmail.com>
2026-01-31 03:51:46 +0300

    ss

---
+++ Assets/Can/Scripts/slideTRAP1.cs
@@ -0,0 +1,40 @@
+using System.Threading;
+using UnityEngine;
+
+public class slideTRAP1 : MonoBehaviour
+{
+   
+    private Vector3 closedPos; 
+    public float speed = 2f;
+    bool isgoing = false;
+    float timer;
+
+    private void Start()
+    {
+         closedPos = transform.position;
+    }
+
+    private void Update()
+    {
+         timer = timer + Time.deltaTime;
+        Debug.Log(timer);
+
+
+        if (timer > 2f)
+        {
+            isgoing = !isgoing;
+            timer = 0f;
+
+        }
+        if (isgoing)
+        {
+           transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+
+        else if (!isgoing)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+    }
+}
