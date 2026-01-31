b3a3090106612a0167ac13c025ec2566e53aee93
trexbest <cndgn3737@gmail.com>
2026-01-31 04:50:28 +0300

    eqwe

---
+++ Assets/Can/Scripts/tpTRap.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using UnityEngine;
+
+public class tpTRap : MonoBehaviour
+{
+    private Vector3 closedPos;
+    public float distance = 2f;
+    int tpState = 0; // 0 = closed, 1 = opening, 2 = open, 
+    float timer;
+
+
+    private void Start()
+    {
+        timer = 0f;
+        tpState = 1;
+        closedPos = transform.position;
+
+
+    }
+
+
+    private void Update()
+    {
+        timer = timer + Time.deltaTime;
+
+        if (timer > 2f)
+        {
+            tpState++;
+            timer = 0f;
+            if (tpState > 2)
+            {
+                tpState = 0;
+            }
+        }
+
+        switch (tpState)
+        {
+            case 0: transform.Translate(closedPos); break;
+            case 1: transform.Translate(closedPos + new Vector3(0, 0, 2)); break;
+            case 2: transform.Translate(closedPos + new Vector3(0, 0, 2)); break;
+
+
+
+        }
+
+    }
+}
